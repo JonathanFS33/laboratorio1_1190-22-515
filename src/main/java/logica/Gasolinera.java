02e7c638c9b2f7d3136b458913e7dad28fdfd8a6
JonathanFS33 <jfrancos6@miumg.edu.gt>
2024-03-02 10:48:53 -0600

    first commit

---
+++ src/main/java/logica/Gasolinera.java
@@ -0,0 +1,37 @@
+package logica;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class Gasolinera {
+    
+    private int bombaSuper = 10000;
+    private int bombaRegular = 9000;
+    private int bombaDiesel = 10000;
+    
+    public int getBombaSuper() {
+        return bombaSuper;
+    }
+
+    public void setBombaSuper(int bombaSuper) {
+        this.bombaSuper = bombaSuper;
+    }
+
+    public int getBombaRegular() {
+        return bombaRegular;
+    }
+
+    public void setBombaRegular(int bombaRegular) {
+        this.bombaRegular = bombaRegular;
+    }
+
+    public int getBombaDiesel() {
+        return bombaDiesel;
+    }
+
+
+    public void setBombaDiesel(int bombaDiesel) {
+        this.bombaDiesel = bombaDiesel;
+    }
+}
